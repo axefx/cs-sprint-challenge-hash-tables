641b1333ef4d9702d9f1608c8abe438afd79d827
Axel <axacorro@gmail.com>
2020-07-10 08:58:18 -0700

    completed get_indices using cache

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,22 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # save weight value as key and index as value
+    cache = dict()
 
+    for i in range(length):
+        weight = weights[i]
+        # for each weight find the difference to the limit
+        # save in cache
+        difference = limit - weight
+        if difference not in cache:
+            cache[weight] = i
+        else:
+            # output: [ 3, 1 ]
+            return [i,cache[difference]]
     return None
+
+if __name__ == "__main__":
+    # [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+    indices = get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], length = 5, limit = 21)
+    print(indices) # output: [ 3, 1 ]
