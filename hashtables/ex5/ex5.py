8d0e2cf6b168e8414b1f9ced35d072cd50602f76
Axel <axacorro@gmail.com>
2020-07-10 10:12:19 -0700

    completed finder function

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,20 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = dict()
+    result = []
+    # set keys as filenames with list of file path as value
+    for file in files:
+        path = file.split("/")
+        filename = path[-1]
+        # handle if not in cache
+        if filename not in cache:
+            cache[filename] = []
+
+        cache[filename].append(file)
+    for q in queries:
+        if q in cache:
+            result += (cache[q])
 
     return result
 
